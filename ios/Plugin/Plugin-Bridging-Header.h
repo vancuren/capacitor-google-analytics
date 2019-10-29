33a53ade2ccad22672b712540fd6c86cafd179aa
Russell Van Curen <russell@vancuren.net>
2019-10-29 18:36:19 -0400

    publish version 0.2.2

---
+++ ios/Plugin/Plugin-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Plugin-Bridging-Header.h
+//  Plugin
+//
+//  Created by Russell Van Curen on 10/29/19.
+//  Copyright © 2019 Max Lynch. All rights reserved.
+//
+
+#ifndef Plugin_Bridging_Header_h
+#define Plugin_Bridging_Header_h
+
+#import <FirebaseCore/FirebaseCore.h>
+
+#endif /* Plugin_Bridging_Header_h */
